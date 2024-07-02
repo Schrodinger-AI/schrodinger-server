dc2fee970cab87a65faa1097d3316cc08b4e6511
house1313 <house@schrodingernft.ai>
2024-07-02 11:49:29 +0800

    feat: change prefix

--- src/SchrodingerServer.Application/Common/PointDispatchConstants.cs
+++ src/SchrodingerServer.Application/Common/PointDispatchConstants.cs
@@ -3,7 +3,7 @@ namespace SchrodingerServer.Common;
 public class PointDispatchConstants
 {
     public const string UNISWAP_PRICE_PREFIX = "UniswapPriceSnapshot-";
-    public const string SYNC_HOLDER_BALANCE_PREFIX = "SyncHolderBalance";
+    public const string CAL_FINISH_PREFIX = "SyncHolderBalance";
     public const string HOLDER_DAILY_CHANGE_HEIGHT_PREFIX = "HolderDailyChangeHeight";
     public const string POINT_ASSEMBLY_TRANSACTION_PREFIX = "PointAssemblyTransaction";
     
