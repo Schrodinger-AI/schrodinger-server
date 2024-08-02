940cf99339a81d6c60c11e2a4f3cd26966cef6eb
house1313 <house@schrodingernft.ai>
2024-08-02 10:34:26 +0800

    feat: recover contest time

--- src/SchrodingerServer.Application/Activity/ActivityApplicationService.cs
+++ src/SchrodingerServer.Application/Activity/ActivityApplicationService.cs
@@ -348,8 +348,8 @@ public class ActivityApplicationService : ApplicationService, IActivityApplicati
         
         return  new StageTimeInDateTime
         {
-            StartTime = new DateTime(2024, 7, 12, 0, 0, 0, DateTimeKind.Utc),
-            EndTime = new DateTime(2024, 7, 17, 23, 59, 59, DateTimeKind.Utc)
+            StartTime = startTime,
+            EndTime = endTime
         };
     }
     
@@ -420,8 +420,8 @@ public class ActivityApplicationService : ApplicationService, IActivityApplicati
         
         return  new StageTimeInDateTime
         {
-            StartTime = new DateTime(2024, 7, 18, 0, 0, 0, DateTimeKind.Utc),
-            EndTime = new DateTime(2024, 7, 18, 23, 59, 59, DateTimeKind.Utc)
+            StartTime = startTime,
+            EndTime = endTime
         };
     }
 
