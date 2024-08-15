c4e34aedc4557b51df54be7d6e52a5ca29a6b38d
house1313 <house@schrodingernft.ai>
2024-08-15 19:25:40 +0800

    feat: fix stage time

--- src/SchrodingerServer.Application/Activity/ActivityApplicationService.cs
+++ src/SchrodingerServer.Application/Activity/ActivityApplicationService.cs
@@ -690,12 +690,14 @@ public class ActivityApplicationService : ApplicationService, IActivityApplicati
             Header = header
         };
         
-        if (stageTime.StartTime < activityBeginTime)
+        DateTime now = DateTime.UtcNow;
+        
+        if (now < activityBeginTime)
         {
             return resp;
         }
         
-        if (stageTime.StartTime > activityBeginTime.AddDays(7) && !input.IsCurrent)
+        if (stageTime.EndTime <= activityBeginTime && !input.IsCurrent)
         {
             return resp;
         }
@@ -804,6 +806,12 @@ public class ActivityApplicationService : ApplicationService, IActivityApplicati
 
         startTime = targetTime;
         endTime = targetTime.AddDays(7);
+        
+        if (!isCurrent)
+        {
+            startTime = startTime.AddDays(-7);
+            endTime = endTime.AddDays(-7);
+        }
         
         return  new StageTimeInDateTime
         {
