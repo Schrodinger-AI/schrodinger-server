404e3e8b9692c619fa2cd19c9b7bb82c907ff60b
house1313 <house@schrodingernft.ai>
2024-08-26 15:54:30 +0800

    feat: remove extra logic

--- src/SchrodingerServer.Application/Users/UserActionProvider.cs
+++ src/SchrodingerServer.Application/Users/UserActionProvider.cs
@@ -133,7 +133,7 @@ public class UserActionProvider : ApplicationService, IUserActionProvider
         {
             if (ecoEarnRewards.Reward.TryGetValue(detail.Symbol, out var value))
             {
-                detail.EcoEarnReward = decimal.Parse(value) * (decimal)0.9;
+                detail.EcoEarnReward = decimal.Parse(value);
             }
             else
             {
