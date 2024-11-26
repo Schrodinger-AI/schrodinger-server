f9cf12203125f8abcabdf0f0adc7aaeb607748f4
house1313 <house@schrodingernft.ai>
2024-11-26 11:41:53 +0800

    feat: add UnderMaintenance Ret

--- src/SchrodingerServer.Application.Contracts/Dtos/Adopts/ImageInfoForDirectAdoptionOutput.cs
+++ src/SchrodingerServer.Application.Contracts/Dtos/Adopts/ImageInfoForDirectAdoptionOutput.cs
@@ -7,4 +7,5 @@ public class ImageInfoForDirectAdoptionOutput
     public string Signature { get; set; }
     public string ImageUri { get; set; }
     public AdoptImageInfo AdoptImageInfo { get; set; }
+    public bool UnderMaintenance { get; set; } = false;
 }
